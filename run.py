d4476e9190313284abe5a47229d9e6b1df2c2c17
welcomemeet <154869445+welcomemeet@users.noreply.github.com>
2025-10-09 11:41:15 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,10 @@
+from app import app, db
+from app.models import User, Task
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Task': Task}
+
+if __name__ == "__main__":
+    app.run(debug=True)
